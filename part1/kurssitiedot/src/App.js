ea99fd7de65e24328e4e675f1d9d24d5a9637be2
Elissei <elissei.hilakivi@gmail.com>
2023-02-23 21:34:51 +0200

    courseinfo

---
+++ part1/kurssitiedot/src/App.js
@@ -0,0 +1,63 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part.name} {props.part.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => <Part key={part.name} part={part} />)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const { parts } = props;
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return (
+    <div>
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
